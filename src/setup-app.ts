03cd4ab0c44cf88c646a7eafdda4d64e5372a75d
Serhii <s.yakovenko@hubmee.com>
2025-05-28 21:16:22 +0300

    add all remove data for testing

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,11 +1,13 @@
 import express, { Express } from 'express';
 import { blogRouters } from './modules/blogs/routers/blog.routers';
+import { testingRoutes } from './testing/testing-routes';
 
 const INIT_ROUTE = '/ht_02/api';
 export const setupApp = (app: Express) => {
   app.use(express.json());
 
   app.use(`${INIT_ROUTE}/blogs`, blogRouters);
+  app.use(`${INIT_ROUTE}/testing/all-data`, testingRoutes);
 
   return app;
 };
---
+++ src/testing/testing-routes.ts
@@ -0,0 +1,10 @@
+import { Router, Response, Request } from 'express';
+import { HttpStatuses } from '../shared/enums/http-statuses';
+import { localBlogsDb } from '../modules/blogs/local-db/local-blogs-db';
+
+export const testingRoutes = Router({});
+
+testingRoutes.delete('', (req: Request, res: Response) => {
+  localBlogsDb.length = 0;
+  res.sendStatus(HttpStatuses.NoContent);
+});
