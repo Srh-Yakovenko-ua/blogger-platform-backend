8f539443aa513555c8a6489d4a70642e5764bea8
Serhii <s.yakovenko@hubmee.com>
2025-07-12 21:00:54 +0300

    remove methods for blog serivce/repository

--- src/modules/blogs/repository/blogs-repository.ts
+++ src/modules/blogs/repository/blogs-repository.ts
@@ -1,36 +1,9 @@
 import { BlogType } from '../types/blog.types';
 import { blogsCollections } from '../../../setup/setup-mongo-db';
-import { InsertOneResult, ObjectId, WithId } from 'mongodb';
-import { PaginationQueryType } from '../../../shared/types/pagination-query-type';
+import { InsertOneResult, ObjectId } from 'mongodb';
 
 // db logic
 export const blogsRepository = {
-  async getBlogs(filtersQuery: PaginationQueryType): Promise<{
-    blogs: WithId<BlogType>[];
-    totalCountBlogs: number;
-  }> {
-    const { searchNameTerm, pageSize, pageNumber, sortBy, sortDirection } = filtersQuery;
-    const skip = (pageNumber - 1) * pageSize;
-    const filter: any = {};
-    if (searchNameTerm) {
-      filter.name = { $regex: searchNameTerm, $options: 'i' };
-    }
-
-    const blogs = await blogsCollections
-      .find(filter)
-      .sort({ [sortBy]: sortDirection })
-      .skip(skip)
-      .limit(pageSize)
-      .toArray();
-
-    const totalCountBlogs = await blogsCollections.countDocuments(filter);
-
-    return { blogs, totalCountBlogs };
-  },
-  async getBlogById(blogID: string): Promise<WithId<BlogType> | null> {
-    const objectID = new ObjectId(blogID);
-    return blogsCollections.findOne({ _id: objectID });
-  },
   async createBlog(data: BlogType): Promise<InsertOneResult<BlogType>> {
     return await blogsCollections.insertOne(data);
   },
--- src/modules/blogs/service/blog.service.ts
+++ src/modules/blogs/service/blog.service.ts
@@ -1,26 +1,7 @@
-import { outputBlogData } from '../utils/output-blog-data';
 import { blogsRepository } from '../repository/blogs-repository';
 import { BlogType } from '../types/blog.types';
-import { Nullable } from '../../../shared/types/nullable';
 
-import { InsertOneResult, WithId } from 'mongodb';
-import { PaginationQueryType } from '../../../shared/types/pagination-query-type';
-// business logic
 export const blogService = {
-  async getBlogs(filtersQuery: PaginationQueryType): Promise<{
-    blogs: WithId<BlogType>[];
-    total: number;
-  }> {
-    const { blogs, totalCountBlogs } = await blogsRepository.getBlogs(filtersQuery);
-    return { blogs: blogs, total: totalCountBlogs };
-  },
-
-  async getBlogById(blogID: string): Promise<Nullable<BlogType>> {
-    const blog = await blogsRepository.getBlogById(blogID);
-    if (blog) return outputBlogData(blog);
-    else return null;
-  },
-
   async createBlog(data: BlogType): Promise<string> {
     const dataWithTimestamp = {
       ...data,
