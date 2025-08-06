920238d088ca5fb6e5e0fe3afd93699247ba939c
Serhii <s.yakovenko@hubmee.com>
2025-08-06 17:13:11 +0300

    fix:regi dto

--- src/modules/auth/dto/registration-dto.ts
+++ src/modules/auth/dto/registration-dto.ts
@@ -1,15 +1,25 @@
 import { body } from 'express-validator';
 
 export const registrationDto = [
-  body('login').trim().isString().notEmpty().withMessage('Login is required'),
+  body('login')
+    .trim()
+    .notEmpty()
+    .withMessage('login is required')
+    .isLength({ min: 3, max: 10 })
+    .withMessage('login must be between 3 and 10 characters')
+    .matches(/^[a-zA-Z0-9_-]*$/)
+    .withMessage('login can contain only letters, numbers, underscores and hyphens'),
 
-  body('password').trim().isString().notEmpty().withMessage('Password is required'),
+  body('password')
+    .notEmpty()
+    .withMessage('password is required')
+    .isLength({ min: 6, max: 20 })
+    .withMessage('password must be between 6 and 20 characters'),
 
   body('email')
     .trim()
     .notEmpty()
-    .isString()
-    .withMessage('Email is required')
-    .isEmail()
+    .withMessage('email is required')
+    .matches(/^[\w\-.]+@([\w-]+\.)+[\w]{2,4}$/)
     .withMessage('Invalid email format'),
 ];
